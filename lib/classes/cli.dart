11853511e8360ec4664158d196799de263a5c546
Adriel <adriel.fsantos@outlook.com>
2023-08-21 14:42:30 -0300

    Adiciona método lerDouble

--- lib/classes/cli.dart
+++ lib/classes/cli.dart
@@ -12,4 +12,41 @@ class Cli {
     if (entrada.isEmpty) return retornoPadrao ?? "";
     return entrada;
   }
+
+  static double lerDouble({
+    String? mensagem,
+    List<bool Function(double num)> validadores = const [],
+  }) {
+    bool entradaInvalida = false;
+    while (true) {
+      if (entradaInvalida) {
+        print("[!] Valor inválido, tente novamente");
+        print("");
+      }
+
+      if (mensagem != null) print(mensagem);
+      String? entrada = stdin.readLineSync(encoding: utf8);
+
+      if (entrada == null) {
+        entradaInvalida = true;
+        continue;
+      }
+      entrada = entrada.trim();
+
+      if (entrada.isEmpty) {
+        entradaInvalida = true;
+        continue;
+      }
+      double? num = double.tryParse(entrada);
+
+      entradaInvalida = num == null || num.isNaN;
+
+      for (var validador in validadores) {
+        if (entradaInvalida) break;
+        entradaInvalida = !validador(num!);
+      }
+
+      if (!entradaInvalida) return num!;
+    }
+  }
 }
