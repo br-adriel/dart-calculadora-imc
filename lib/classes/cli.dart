ee9d7ce5bf553e34071687f10a6f76ccb9ab2b98
Adriel <adriel.fsantos@outlook.com>
2023-08-21 13:09:11 -0300

    Adiciona retornoPadrao a método lerString

--- lib/classes/cli.dart
+++ lib/classes/cli.dart
@@ -2,8 +2,14 @@ import 'dart:convert';
 import 'dart:io';
 
 class Cli {
-  static String lerString({String? mensagem}) {
+  static String lerString({String? mensagem, String? retornoPadrao}) {
     if (mensagem != null) print(mensagem);
-    return stdin.readLineSync(encoding: utf8) ?? "";
+    String? entrada = stdin.readLineSync(encoding: utf8);
+
+    if (entrada == null) return retornoPadrao ?? "";
+    entrada = entrada.trim();
+
+    if (entrada.isEmpty) return retornoPadrao ?? "";
+    return entrada;
   }
 }
